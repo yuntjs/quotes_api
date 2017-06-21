f92e84ebaa80a7d233099c64c49ba3207778898e
Tae Yun <yuntjs@gmail.com>
2017-06-21 15:27:22 -0400

    creates seed file

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development do
 
   gem 'rspec-rails', '~> 3.5'
 
+  gem 'faker'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..500).each do |x|
+  fn = Faker::Name.name
+  qu = Faker::Hipster.sentence
+  Quote.create(author: fn, content: qu)
+end
