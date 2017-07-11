0f68bc6083f9c7947153e63fe4c3fc750df64051
Tae Yun <yuntjs@gmail.com>
2017-07-11 11:08:02 -0400

    fix

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,5 @@ ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amaz
   git clone https://github.com/yuntjs/quotes_ui.git
   git clone https://github.com/yuntjs/quotes_api.git
   git clone https://github.com/yuntjs/quotes-provision-prod.git
-  docker build -t="quotesapi" ./quotes_api
-  docker login -u $DOCKER_USER -p $DOCKER_PASS
-  docker tag quotesui:latest taejunyun/quotesapi
-  docker push taejunyun/quotesapi
   docker stack deploy -c /home/docker/quotes-provision-prod/docker-compose.yml app
 ENDSSH
