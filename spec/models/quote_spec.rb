5c00077cdb3614efb0a1a31590dbb09b7c72875e
Tae Yun <yuntjs@gmail.com>
2017-07-11 10:44:03 -0400

    configure datbase, spec and deploy

--- spec/models/quote_spec.rb
+++ spec/models/quote_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Quote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:quote) { Quote.new(content: "To be or not to be", author: "Tae jun Yun") }
+
+  it "has content" do
+    expect(quote.content).to eq "To be or not to be"
+  end
+
+  it "has an author" do
+    expect(quote.author).to eq "Tae jun Yun"
+  end
+
 end
