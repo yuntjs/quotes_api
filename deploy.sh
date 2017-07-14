5d34193f68ea4c258391b9b29e26697db112f15a
Tae Yun <yuntjs@gmail.com>
2017-07-14 15:10:57 +0000

    makes manger ip variable in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ssh -o "StrictHostKeyChecking no" docker@ec2-52-14-238-30.us-east-2.compute.amazonaws.com env TAG=0.1.$CIRCLE_BUILD_NUM DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
+ssh -o "StrictHostKeyChecking no" docker@$MANAGER_IP env TAG=0.1.$CIRCLE_BUILD_NUM DOCKER_USER=$DOCKER_USER DOCKER_PASS=$DOCKER_PASS 'bash -s' <<'ENDSSH'
   docker service update --image taejunyun/quotesapi:$TAG --force app_web
 ENDSSH
